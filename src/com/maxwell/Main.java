640de1d8693811cc95a33db366359db56752b2bc
MrNimm <79066951+MrNimm@users.noreply.github.com>
2021-04-09 20:04:09 -0600

    Trying to get an array to work

--- src/com/maxwell/Main.java
+++ src/com/maxwell/Main.java
@@ -1,7 +1,10 @@
 package com.maxwell;
 
 import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.IOException;
 
 
 //The program will take each cost and mark it up 30% using the formula (itemCost * 1.3) where itemCost is your own declared variable for product cost.
@@ -26,6 +29,9 @@ import java.util.Scanner;
 //You will also need documentation. Take a screen capture of your program’s execution and put it in a Word document or PDF. Upload this along with your program files in the Assignment Submission Folder. In Windows, hold the Alt key while pressing the print screen button (PrtScn) on the keyboard. A picture of the active window will be stored in memory. Go to Edit-> Paste in your Word document and a screen capture will appear.
 //You will submit your .java and screen capture to the Assignment Submission Folder. If you compress these be sure to use ONLY a single ZIP file. You will not submit your work as a .rar or any other type of compressed file format.
 
+//read String and OG cost together
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -40,16 +46,41 @@ public class Main {
                     System.out.println(contentline);
                 }
 
-                //convert string to int
-                int y = Integer.parseInt(contentline);
+            while (fileScanner.hasNextLine()) {
 
-               // write += ("" + y * 1.3);
+                String data = fileScanner.nextLine();
+                String itemName;
+                double originalCost;
+                double markupCost;
 
-                System.out.println("" + y);
-                br.close();
+
+                //    double[] myList = new double[10];
+
+                double [] myList = {439.65, 40.00, 525.99, 139.65, 89.94, 189.94, 29.94, 19.94, 69.94, 49.94};
+
+                System.out.println(myList);
+
+            //    fileScanner.close();
+
             }
-        } catch (Exception exception) {
-            return;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
+
+
+    /*try {
+                        originalCost = Double.parseDouble(data);
+                        markupCost = Double.parseDouble(data) * 1.3;
+                    } catch (NumberFormatException e) {
+                        itemName = data;*/
+//                System.out.println(fileScanner.nextLine());
+//                System.out.println(itemName + originalCost);
+
+//                try {
+//                    originalCost = Double.parseDouble(data);
+//                    markupCost = Double.parseDouble(data) * 1.3;
+//                } catch (NumberFormatException e) {
+//                    itemName = data;
+//                }
